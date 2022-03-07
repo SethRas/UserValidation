cb8cf176db22e7b91d58b31742c641b878f0a08f
rasset69@gmail.com <rasset69@gmail.com>
2022-03-07 08:53:09 -0700

    Add project files.

---
+++ UserValidation/UserValidation.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserValidation
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Submit = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.AgeTextBox3 = New System.Windows.Forms.TextBox()
+        Me.AgeLabel3 = New System.Windows.Forms.Label()
+        Me.LastNameTextBox2 = New System.Windows.Forms.TextBox()
+        Me.LastNameLabel2 = New System.Windows.Forms.Label()
+        Me.FirstNAmeTextBox1 = New System.Windows.Forms.TextBox()
+        Me.FirstNAmeLabel1 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.GroupBox2)
+        Me.GroupBox1.Controls.Add(Me.AgeTextBox3)
+        Me.GroupBox1.Controls.Add(Me.AgeLabel3)
+        Me.GroupBox1.Controls.Add(Me.LastNameTextBox2)
+        Me.GroupBox1.Controls.Add(Me.LastNameLabel2)
+        Me.GroupBox1.Controls.Add(Me.FirstNAmeTextBox1)
+        Me.GroupBox1.Controls.Add(Me.FirstNAmeLabel1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(382, 329)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Submit)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Location = New System.Drawing.Point(0, 226)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(381, 102)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(276, 37)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 42)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "E&xit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Submit
+        '
+        Me.Submit.Location = New System.Drawing.Point(146, 37)
+        Me.Submit.Name = "Submit"
+        Me.Submit.Size = New System.Drawing.Size(75, 42)
+        Me.Submit.TabIndex = 3
+        Me.Submit.Text = "C&lear"
+        Me.Submit.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(22, 37)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 42)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "&Submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'AgeTextBox3
+        '
+        Me.AgeTextBox3.Location = New System.Drawing.Point(90, 77)
+        Me.AgeTextBox3.Name = "AgeTextBox3"
+        Me.AgeTextBox3.Size = New System.Drawing.Size(100, 22)
+        Me.AgeTextBox3.TabIndex = 7
+        '
+        'AgeLabel3
+        '
+        Me.AgeLabel3.AutoSize = True
+        Me.AgeLabel3.Location = New System.Drawing.Point(6, 77)
+        Me.AgeLabel3.Name = "AgeLabel3"
+        Me.AgeLabel3.Size = New System.Drawing.Size(33, 17)
+        Me.AgeLabel3.TabIndex = 6
+        Me.AgeLabel3.Text = "Age"
+        '
+        'LastNameTextBox2
+        '
+        Me.LastNameTextBox2.Location = New System.Drawing.Point(90, 49)
+        Me.LastNameTextBox2.Name = "LastNameTextBox2"
+        Me.LastNameTextBox2.Size = New System.Drawing.Size(100, 22)
+        Me.LastNameTextBox2.TabIndex = 5
+        '
+        'LastNameLabel2
+        '
+        Me.LastNameLabel2.AutoSize = True
+        Me.LastNameLabel2.Location = New System.Drawing.Point(6, 49)
+        Me.LastNameLabel2.Name = "LastNameLabel2"
+        Me.LastNameLabel2.Size = New System.Drawing.Size(72, 17)
+        Me.LastNameLabel2.TabIndex = 4
+        Me.LastNameLabel2.Text = "LastName"
+        '
+        'FirstNAmeTextBox1
+        '
+        Me.FirstNAmeTextBox1.Location = New System.Drawing.Point(90, 21)
+        Me.FirstNAmeTextBox1.Name = "FirstNAmeTextBox1"
+        Me.FirstNAmeTextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.FirstNAmeTextBox1.TabIndex = 3
+        '
+        'FirstNAmeLabel1
+        '
+        Me.FirstNAmeLabel1.AutoSize = True
+        Me.FirstNAmeLabel1.Location = New System.Drawing.Point(6, 21)
+        Me.FirstNAmeLabel1.Name = "FirstNAmeLabel1"
+        Me.FirstNAmeLabel1.Size = New System.Drawing.Size(72, 17)
+        Me.FirstNAmeLabel1.TabIndex = 1
+        Me.FirstNAmeLabel1.Text = "FirstName"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Location = New System.Drawing.Point(425, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(352, 328)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "GroupBox3"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Result"
+        '
+        'UserValidation
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "UserValidation"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Submit As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents AgeTextBox3 As TextBox
+    Friend WithEvents AgeLabel3 As Label
+    Friend WithEvents LastNameTextBox2 As TextBox
+    Friend WithEvents LastNameLabel2 As Label
+    Friend WithEvents FirstNAmeTextBox1 As TextBox
+    Friend WithEvents FirstNAmeLabel1 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ UserValidation/UserValidation.vb
@@ -0,0 +1,95 @@
+﻿Public Class UserValidation
+
+    Sub validateInput()
+        Dim UserAge As Integer
+        'If AgeTextBox3.Text = "" Then
+        '    accumulateMessage("AGE is required")
+        '    AgeTextBox3.Focus()
+        'End If
+
+        Try
+            UserAge = CInt(AgeTextBox3.Text)
+        Catch ex As Exception
+            accumulateMessage("AGE must be a valid entry")
+            AgeTextBox3.Focus()
+        End Try
+
+        If LastNameTextBox2.Text = "" Then
+            accumulateMessage("LAST name is required")
+            LastNameTextBox2.Focus()
+        End If
+
+        If FirstNAmeTextBox1.Text = "" Then
+            accumulateMessage("FIRST name is required")
+            FirstNAmeTextBox1.Focus()
+        End If
+
+        If accumulateMessage() <> "" Then
+            MsgBox(accumulateMessage())
+            accumulateMessage(, True)
+        End If
+
+    End Sub
+
+    Private Function accumulateMessage(Optional newMessage As String = "", Optional clear As Boolean = False) As String
+        Static _message As String
+        Select Case clear
+            Case False
+                If newMessage <> "" Then
+                    _message &= newMessage & vbCrLf
+                End If
+
+            Case Else
+                _message = ""
+        End Select
+
+        Return _message
+    End Function
+
+    Private Sub Reset()
+        'reset All form controls to default
+        FirstNAmeTextBox1.Text = ""
+        LastNameTextBox2.Text = ""
+        AgeTextBox3.Text = ""
+        'Clear any stored messages
+        accumulateMessage(, True)
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles FirstNAmeLabel1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles LastNameLabel2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles AgeLabel3.Click
+
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles LastNameTextBox2.TextChanged
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        validateInput()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Submit_Click(sender As Object, e As EventArgs) Handles Submit.Click
+        Reset()
+    End Sub
+End Class
